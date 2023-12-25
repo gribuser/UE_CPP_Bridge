4f1f7285adaf09866f9efd7085d8de52432ff6be
PJ <pjko@ya.ru>
2023-12-26 00:12:44 +0300

    Ctrl+S + FMath::RandRange + TSharedRef + TWeakPtr + FIntVector3 + FRotator + PI

--- Source/UE_CPP_Bridge/Public/UE_BridgeMap.h
+++ Source/UE_CPP_Bridge/Public/UE_BridgeMap.h
@@ -2,7 +2,10 @@
 #include "UE_CPP_Bridge_Setup.h"
 
 #if UE_CPP_BRIDGE_CONTAINER_CLASSES_MODE == 1
+
+#define _ENFORCE_MATCHING_ALLOCATORS 0
 #include <map>
+
 //static_assert(0, "Not implemented yet");
 #elif UE_CPP_BRIDGE_CONTAINER_CLASSES_MODE == 2
 #include "Containers/Map.h"
@@ -12,11 +15,46 @@ static_assert(0, "Unknown implementation ID, see UE_CPP_BRIDGE_CONTAINER_CLASSES
 
 // We emulate base UE's TMap functionality with std::*
 #if UE_CPP_BRIDGE_CONTAINER_CLASSES_MODE == 1
+
+
 template<typename InKeyType, typename InElementType>
-class TMap: public std::map<InKeyType, InElementType> {
+class P2PPair : private std::pair<InKeyType, InElementType> {
+public:
+	using std::pair<InKeyType, InElementType>::first;
+	using std::pair<InKeyType, InElementType>::second;
+	const InKeyType& Key() {
+		return first;
+	}
+	InElementType Value() {
+		return second;
+	}
+};
+
+template<typename InKeyType, typename InElementType>
+class TMap: public std::map<
+	InKeyType,
+	InElementType,
+	std::less<InKeyType>,
+	std::allocator<P2PPair<const InKeyType, InElementType>>
+> {
 public:
 	using TMap<InKeyType, InElementType>::TMap;
 	//InElementType& operator[](InKeyType idx)             { return TMap<InKeyType, InElementType>::TMap[idx]; }
 	//const InElementType& operator[](InKeyType idx) const { return TMap<InKeyType, InElementType>::TMap[idx]; }
+	bool Contains(const InKeyType& Key) {
+		return contains(Key);
+	}
+
+	InElementType& Emplace(InKeyType&& Key, InElementType&& InVal) {
+		auto Pair = emplace(Key, InVal);
+		return Pair.second;
+	}
+
+	InElementType* Find(const InKeyType& Key) {
+		return find(Key);
+	}
+	const InElementType* Find(const InKeyType& Key) const {
+		return find(Key);
+	}
 };
 #endif
